c1ab8c8a4eb40537d16a64d11f3f02d899dc18d2
HyunsunRyu <lovedangyou@gmail.com>
2017-07-13 16:57:15 +0900

    ss

--- NodeJsTutorial/Assets/Scripts/GameManager.cs
+++ NodeJsTutorial/Assets/Scripts/GameManager.cs
@@ -5,55 +5,58 @@ using SocketIO;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] private GameObject playerPfb;
+    public static GameManager Instance { get; private set; }
+
+    [SerializeField] private GameObject userPfb;
 
     private SocketIOComponent socket;
-    private MiniPool<PlayerController> playerPool;
+    private MiniPool<UserController> userObjectPool;
 
-    private Dictionary<string, PlayerController> playerList;
+    private Dictionary<string, UserController> userDic;
+    private UserController player;
 
-    private void Start()
+    private void Awake()
     {
+        Instance = this;
+
         GameObject hideRoot = new GameObject("HideRoot");
         hideRoot.transform.parent = transform;
 
-        playerPool = MiniPool<PlayerController>.MakeMiniPool(hideRoot.transform, playerPfb);
+        userObjectPool = MiniPool<UserController>.MakeMiniPool(hideRoot.transform, userPfb);
 
         socket = SocketIOComponent.instance;
-        
+
         socket.On("error", ErrorCallback);
 
+        socket.On("SUCCESS_ACCESS", OnSuccessAccess);
         socket.On("SUCCESS_CONNECT", OnSuccessConnect);
         socket.On("USER_CONNECTED", OnUserConnected);
         socket.On("USER_DISCONNECTED", OnUserDisConnected);
         socket.On("MOVE", OnUserMove);
 
-        playerList = new Dictionary<string, PlayerController>();
+        userDic = new Dictionary<string, UserController>();
 
-        socket.Connect();
+        StartCoroutine(TryConnect());
     }
 
+    private IEnumerator TryConnect()
+    {
+        socket.Connect();
+        yield return new WaitForSeconds(0.5f);
+        Connect();
+    }
+    
     private void ErrorCallback(SocketIOEvent e)
     {
         Debug.LogError("error : " + e.name + " / " +  e.data);
     }
-
-    private void Update()
+    
+    private void Connect()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-            Connect();
-    }
+        SpawnData spawnPoint = new SpawnData(GetRandomPosition());
 
-    public void Connect()
-    {
-        UserData newUser = new UserData();
-        newUser.id = "null";
-        newUser.name = "Test";
-        newUser.position = GetRandomPosition();
-
-        string data = JsonUtility.ToJson(newUser);
-        socket.Emit("USER_CONNECT", new JSONObject(data));
-        Debug.Log("emit - user_connect : " + data);
+        string data = JsonUtility.ToJson(spawnPoint);
+        socket.Emit("USER_ACCESS", new JSONObject(data));
     }
 
     private float[] GetRandomPosition()
@@ -64,34 +67,68 @@ public class GameManager : MonoBehaviour
         pos[2] = Random.Range(245f, 255f);
         return pos;
     }
+
+    private void OnSuccessAccess(SocketIOEvent e)
+    {
+        Debug.Log("Success Access : " + e.data.ToString());
+
+        UserData playerData = JsonUtility.FromJson<UserData>(e.data.ToString());
+        playerData.name = "TEST";
+        player = CreateUser(playerData, true);
+        userDic.Add(playerData.id, player);
+
+        string data = JsonUtility.ToJson(playerData);
+        socket.Emit("USER_CONNECT", new JSONObject(data));
+    }
     
     private void OnSuccessConnect(SocketIOEvent e)
     {
-        Debug.Log(e.data.ToString());
+        Debug.Log("Success Connect : " + e.data.ToString());
 
         UserDataList userList = JsonUtility.FromJson<UserDataList>(e.data.ToString());
         foreach (UserData user in userList.users)
         {
-            Debug.Log(user.name + " // " + user.GetPostion());
+            userDic.Add(user.id, CreateUser(user, false));
         }
-
-        //UserData player = JsonUtility.FromJson<UserData>(e.data.ToString());
-        //Debug.Log(player.name + " :: " + player.GetPostion());
     }
 
     private void OnUserConnected(SocketIOEvent e)
     {
         Debug.Log(e.data.ToString());
 
-        UserData player = JsonUtility.FromJson<UserData>(e.data.ToString());
-        Debug.Log(player.name + " :: " + player.GetPostion());
+        UserData userData = JsonUtility.FromJson<UserData>(e.data.ToString());
+        userDic.Add(userData.id, CreateUser(userData, false));
     }
 
     private void OnUserDisConnected(SocketIOEvent e)
+    {
+        UserData userData = JsonUtility.FromJson<UserData>(e.data.ToString());
+
+        DestroyUser(userData);
+    }
+
+    public void MovedPlayer(Vector3 pos)
     {
     }
 
     private void OnUserMove(SocketIOEvent e)
     {
+    }
+
+    private UserController CreateUser(UserData data, bool bIsUser = false)
+    {
+        UserController user = userObjectPool.GetItem();
+        user.Init(data, bIsUser);
+        return user;
+    }
+
+    private void DestroyUser(UserData data)
+    {
+        if (!userDic.ContainsKey(data.id))
+            Debug.LogError("something is wrong : the user whom has this id is none");
+        else
+        {
+            userObjectPool.ReturnItem(userDic[data.id]);
+        }
     }
 }
--- index.js
+++ index.js
@@ -5,27 +5,51 @@ var io = require('socket.io').listen(server);
 
 app.set('port', process.env.PORT || 3000);
 
-var clients = [];
+var userList = [];
 
 io.on("connection", function(socket){
 
   var currentUser;
 
-  socket.on("USER_CONNECT", function(data){
+  socket.on("USER_ACCESS", function(data){
+
+    var spawnPoint = [];
+
+    if(userList.length === 0) {
+      spawnPoint[0] = 250.0;
+      spawnPoint[1] = 0.0;
+      spawnPoint[2] = 250.0;
+    }
+    else {
+      userList.forEach(function(value, index, ar){
+
+      });
+    }
 
     currentUser = {
       id:guid(),
+      name:"",
+      position:spawnPoint
+    };
+
+    //userList.push(currentUser);
+
+    socket.emit("SUCCESS_ACCESS", currentUser);
+    //socket.emit("SUCCESS_ACCESS", {"users":userList});
+  });
+
+  socket.on("USER_CONNECT", function(data){
+
+    currentUser = {
+      id:data.id,
       name:data.name,
       position:data.position
     };
 
-    console.log(data.position + " : " + currentUser.position);
+    userList.push(currentUser);
 
-    clients.push(currentUser);
-
-    console.log("User : " + currentUser.name + " is connected");
+    socket.emit("SUCCESS_CONNECT", currentUser);
     socket.broadcast.emit("USER_CONNECTED", currentUser);
-    socket.emit("SUCCESS_CONNECT", {"users":clients});
   });
 
   socket.on("MOVE", function(data){
@@ -36,10 +60,10 @@ io.on("connection", function(socket){
 
   socket.on("disconnect", function(){
     socket.broadcast.emit("USER_DISCONNECTED", currentUser);
-    for(var i=0;i<clients.length; i++){
-      if(clients[i].name == currentUser.name){
-        console.log("User " + clients[i].name + " disconnected");
-        clients.splice(i, 1);
+    for(var i=0;i<userList.length; i++){
+      if(userList[i].name == currentUser.name){
+        console.log("User " + userList[i].name + " disconnected");
+        userList.splice(i, 1);
       }
     }
   });
@@ -51,10 +75,21 @@ server.listen(app.get('port'), function(){
 
 //guid 생성하는 코드. //
 // https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript#comment29646908_873856 //
+/*
 function guid() {
     function _p8(s) {
         var p = (Math.random().toString(16)+"000000000").substr(2,8);
         return s ? "-" + p.substr(0,4) + "-" + p.substr(4,4) : p ;
     }
     return _p8() + _p8(true) + _p8(true) + _p8();
+}
+*/
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
 }
