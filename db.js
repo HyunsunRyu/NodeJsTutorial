efefb29aa69cdca7403eafac8380055b7fa9d6fc
HyunsunRyu <lovedangyou@gmail.com>
2017-09-10 19:54:23 +0900

    nodejs test code

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const sqlite3 = require('sqlite3').verbose();
 var date = require('node.date-time');
 let db = new sqlite3.Database('./db/sample.db');
 
-var insertTime = function(time)
+exports.InsertTime = function(time)
 {
   db.serialize(function(){
     db.run('create table if not exists testDB(id integer primary key not null, data varchar(50))');
@@ -16,4 +16,16 @@ var insertTime = function(time)
   console.log(time);
 }
 
-exports.InsertTime = insertTime;
+exports.GetList = function(){
+  db.serialize(function(){
+    db.run('create table if not exists testDB(id integer primary key not null, data varchar(50))');
+    let playlistid = 1;
+    db.get('select d as data from testDB', [playlistid], function(err, row){
+      if(err){
+        return console.log(err.message);
+      }
+      console.log(row.id);
+    });
+  });
+  db.close();
+}
